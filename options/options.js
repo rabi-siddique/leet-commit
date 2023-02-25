c67eebb5fa16a811fb591fc76292d4fe31f0dbe2
rabi27 <rabisiddique27@gmail.com>
2023-02-25 16:59:51 +0500

    API call to get all databases and add permission for notion.com to avoid CORs error

--- options/options.js
+++ options/options.js
@@ -1,3 +1,5 @@
+/*------------- Allow User to Save their API key   -------------*/
+
 const apiKeyInput = document.getElementById('api-key-input');
 const saveButton = document.getElementById('save-button');
 
@@ -25,5 +27,35 @@ const checkInput = () => {
 
 // Add event listener to the input field to check its length
 apiKeyInput.addEventListener('input', checkInput);
+
 // Check input field on page load in case the user has already entered an API key
 checkInput();
+
+/*------------- Get and Show All Databases   -------------*/
+
+// Get the list element to display the databases
+const databaseList = document.getElementById('database-list');
+
+// Check if the API key is already saved
+chrome.storage.sync.get('apiKey', ({ apiKey }) => {
+  if (apiKey) {
+    // Make a GET request to the /databases endpoint to load all the databases
+    fetch('https://api.notion.com/v1/databases', {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        'Notion-Version': '2021-08-16',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Display the databases in the list element
+        const databaseItems = data.results.map(
+          (database) => `<li>${database.title[0].text.content}</li>`
+        );
+        databaseList.innerHTML = databaseItems.join('');
+      })
+      .catch((error) => {
+        console.error('Error loading databases:', error);
+      });
+  }
+});
