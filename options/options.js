8d4a1a592f573d386fe2f4e069ea5c4673b664f4
rabi27 <rabisiddique27@gmail.com>
2023-02-25 22:54:14 +0500

    pivot to github extension

--- options/options.js
+++ options/options.js
@@ -1,61 +1,49 @@
-/*------------- Allow User to Save their API key   -------------*/
-
 const apiKeyInput = document.getElementById('api-key-input');
+const ownerInput = document.getElementById('owner-input');
+const repoInput = document.getElementById('repo-input');
 const saveButton = document.getElementById('save-button');
 
-// Load the saved API key (if any) when the options page is opened
-chrome.storage.sync.get('apiKey', ({ apiKey }) => {
-  if (apiKey) {
-    apiKeyInput.value = apiKey;
+// Load the saved API key, owner, and repo (if any) when the options page is opened
+chrome.storage.sync.get(
+  ['apiKey', 'owner', 'repo'],
+  ({ apiKey, owner, repo }) => {
+    if (apiKey) {
+      apiKeyInput.value = apiKey;
+    }
+    if (owner) {
+      ownerInput.value = owner;
+    }
+    if (repo) {
+      repoInput.value = repo;
+    }
   }
-});
+);
 
-// Save the API key when the user clicks the save button
+// Save the API key, owner, and repo when the user clicks the save button
 saveButton.addEventListener('click', () => {
   const apiKey = apiKeyInput.value;
-  chrome.storage.sync.set({ apiKey });
+  const owner = ownerInput.value;
+  const repo = repoInput.value;
+  chrome.storage.sync.set({ apiKey, owner, repo });
 });
 
-// Check if the input field is empty or contains less than 50 characters
+// Check if the input fields are empty or contain less than 50 characters
 const checkInput = () => {
-  if (apiKeyInput.value.length === 0 || apiKeyInput.value.length < 50) {
-    saveButton.disabled = true;
-  } else {
+  if (
+    apiKeyInput.value.length === 0 ||
+    ownerInput.value.length === 0 ||
+    repoInput.value.length === 0
+  ) {
     saveButton.disabled = false;
+  } else {
+    saveButton.disabled = true;
   }
 };
 
-// Add event listener to the input field to check its length
+// Add event listener to the input fields to check their length
 apiKeyInput.addEventListener('input', checkInput);
+ownerInput.addEventListener('input', checkInput);
+repoInput.addEventListener('input', checkInput);
 
-// Check input field on page load in case the user has already entered an API key
+// Check input fields on page load in case the user has already entered data
 checkInput();
-
-/*------------- Get and Show All Databases   -------------*/
-
-// Get the list element to display the databases
-const databaseList = document.getElementById('database-list');
-
-// Check if the API key is already saved
-chrome.storage.sync.get('apiKey', ({ apiKey }) => {
-  if (apiKey) {
-    // Make a GET request to the /databases endpoint to load all the databases
-    fetch('https://api.notion.com/v1/databases', {
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-        'Notion-Version': '2021-08-16',
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // Display the databases in the list element
-        const databaseItems = data.results.map(
-          (database) => `<li>${database.title[0].text.content}</li>`
-        );
-        databaseList.innerHTML = databaseItems.join('');
-      })
-      .catch((error) => {
-        console.error('Error loading databases:', error);
-      });
-  }
-});
