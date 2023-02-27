fcd6e65d20f9949bb7db65ac567c8e7c19eac6bb
rabi27 <rabisiddique27@gmail.com>
2023-02-27 14:24:50 +0500

    http request to github api to commit code

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,17 @@
+// Global Credentials
+let apiKey;
+let owner;
+let repo;
+let sourceCode;
+
 /* -------------------- Receiving Directories -------------------- */
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.message === 'send-directories') {
     const directories = request.data[0];
-    const apiKey = request.data[1];
+    apiKey = request.data[1];
+    owner = request.data[2];
+    repo = request.data[3];
     const select = document.getElementById('directory-select');
 
     // Add each directory as an option in the select element
@@ -21,8 +29,64 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 // And responds by sending a message to all active popups with the directories.
 chrome.runtime.sendMessage({ message: 'send-message' });
 
-/* -------------------- Receiving Code -------------------- */
+/* -------------------- Commit Code to Github -------------------- */
 
-chrome.storage.sync.get(['code'], ({ code }) => {
-  const code = code;
+// Get the commit button element
+const commitButton = document.getElementById('commit-button');
+
+// Add a click event listener to the commit button
+commitButton.addEventListener('click', () => {
+  // Get the directory select and filename input elements
+  const directorySelect = document.getElementById('directory-select');
+  const filenameInput = document.getElementById('filename-input');
+
+  // Get the selected directory and entered filename
+  const selectedDirectory = directorySelect.value;
+  const filename = filenameInput.value;
+
+  // Get the commit message input element and entered commit message
+  const commitMessageInput = document.getElementById('commit-message-input');
+  const commitMessage = commitMessageInput.value;
+
+  // Construct the API endpoint URL for creating a new file
+  const path = `${selectedDirectory}/${filename}`;
+  const endpoint = `https://api.github.com/repos/${owner}/${repo}/contents/${path}`;
+
+  // Encode file content to base64
+  chrome.storage.sync.get(['code'], ({ code }) => {
+    sourceCode = code;
+    const content = btoa(sourceCode);
+
+    // Construct the request object
+    const request = {
+      method: 'PUT',
+      headers: {
+        Authorization: `token ${apiKey}`,
+        'Content-Type': 'application/json',
+        Accept: 'application/vnd.github.v3+json',
+      },
+      body: JSON.stringify({
+        message: commitMessage,
+        content: content,
+        path: path,
+      }),
+    };
+
+    // Make the HTTP request to the GitHub API
+    fetch(endpoint, request)
+      .then((response) => {
+        if (response.ok) {
+          console.log('Code Commit Success');
+        } else {
+          console.log('Error occurred');
+        }
+      })
+      .catch((error) => {
+        // An error occurred while making the HTTP request
+        console.error(
+          'An error occurred while making the HTTP request.',
+          error
+        );
+      });
+  });
 });
